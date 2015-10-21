c0c57717fdcdd8fa3b70c184d42017ac1a2ad4da
David Light <dlight@beanstream.com>
2015-10-21 14:39:57 -0700

    MDI-434 Initial commit of simulator

---
+++ BeanstreamAPISimulator/BeanstreamAPISimulator/BeanstreamAPISimulator.h
@@ -0,0 +1,14 @@
+//
+//  BeanstreamAPISimulator.h
+//  BeanstreamAPISimulator
+//
+//  Created by DLight on 2015-10-20.
+//  Copyright © 2015 Beanstream. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BICBeanstreamAPI.h"
+
+@interface BeanstreamAPISimulator : BICBeanstreamAPI
+
+@end
---
+++ BeanstreamAPISimulator/BeanstreamAPISimulator/BeanstreamAPISimulator.m
@@ -0,0 +1,13 @@
+//
+//  BeanstreamAPISimulator.m
+//  BeanstreamAPISimulator
+//
+//  Created by DLight on 2015-10-20.
+//  Copyright © 2015 Beanstream. All rights reserved.
+//
+
+#import "BeanstreamAPISimulator.h"
+
+@implementation BeanstreamAPISimulator
+
+@end
