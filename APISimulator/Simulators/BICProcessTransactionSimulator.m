650e43fcf3106769007388bf1a5dcfa5001781b8
Sven Resch <sven.resch@beanstream.com>
2016-01-15 16:51:47 -0800

    MDI-442 Updated to be close to parity with Android implementation of most Process Transaction modes of operation.

--- APISimulator/Simulators/BICProcessTransactionSimulator.m
+++ APISimulator/Simulators/BICProcessTransactionSimulator.m
@@ -14,41 +14,283 @@
 
 @implementation BICProcessTransactionSimulator
 
+static NSString *PROCESS_TRANSACTION_VERSION_NUMBER = @"1.0";
+static NSString *SIGNATURE_REQUIRED_FLAG = @"0";
+static NSString *SIGNATURE_NOT_REQUIRED_FLAG = @"1";
+
+static BICSimulatorMode *SimModeProcessTxnApproved = nil;
+static BICSimulatorMode *SimModeProcessTxnApprovedSigRequired = nil;
+static BICSimulatorMode *SimModeProcessTxnDeclined = nil;
+static BICSimulatorMode *SimModeProcessTxnDeclinedAmountTooHigh = nil;
+static BICSimulatorMode *SimModeProcessTxnDeclinedRefundLimitExceeded = nil;
+static BICSimulatorMode *SimModeProcessTxnDeclinedMissingInvalidAdjutmentId = nil;
+static BICSimulatorMode *SimModeProcessTxnDeclinedSessionExpired = nil;
+static BICSimulatorMode *SimModeProcessTxnDeclinedSessionInvalid = nil;
+static BICSimulatorMode *SimModeProcessTxnDeclinedCompletionGreaterThanReserve = nil;
+static BICSimulatorMode *SimModeProcessTxnDeclinedApplicationError = nil;
+static BICSimulatorMode *SimModeProcessTxnDeclinedReferralResponse = nil;
+//static BICSimulatorMode *SimModeProcessTxnDeclinedCryptoFailure = nil;
+static BICSimulatorMode *SimModeProcessTxnDeclinedServNotAllowed = nil;
+static BICSimulatorMode *SimModeProcessTxnDeclinedNotComplete = nil;
+static BICSimulatorMode *SimModeProcessTxnDeclinedErrorHTTP = nil;
+static BICSimulatorMode *SimModeProcessTxnDeclinedErrorNetwork = nil;
+
 @synthesize simulatorMode, interactive;
+
+#pragma mark - Initialization methods
+
++ (void)initialize
+{
+    SimModeProcessTxnApproved = [[BICSimulatorMode alloc] initWithLabel:@"Approved"];
+    SimModeProcessTxnApprovedSigRequired = [[BICSimulatorMode alloc] initWithLabel:@"Approved - Signature"];
+    SimModeProcessTxnDeclined = [[BICSimulatorMode alloc] initWithLabel:@"Declined"];
+    SimModeProcessTxnDeclinedAmountTooHigh = [[BICSimulatorMode alloc] initWithLabel:@"Amount Too High"];
+    SimModeProcessTxnDeclinedRefundLimitExceeded = [[BICSimulatorMode alloc] initWithLabel:@"Refund Limit Exceeded"];
+    SimModeProcessTxnDeclinedMissingInvalidAdjutmentId = [[BICSimulatorMode alloc] initWithLabel:@"Missing/Invalid Adjustment Id"];
+    SimModeProcessTxnDeclinedSessionExpired = [[BICSimulatorMode alloc] initWithLabel:@"Session Expired"];
+    SimModeProcessTxnDeclinedSessionInvalid = [[BICSimulatorMode alloc] initWithLabel:@"Session Invalid"];
+    SimModeProcessTxnDeclinedCompletionGreaterThanReserve = [[BICSimulatorMode alloc] initWithLabel:@"PAC Completion Greater Than Reserve"];
+    SimModeProcessTxnDeclinedApplicationError = [[BICSimulatorMode alloc] initWithLabel:@"Application Error"];
+    SimModeProcessTxnDeclinedReferralResponse = [[BICSimulatorMode alloc] initWithLabel:@"Referral"];
+    //SimModeProcessTxnDeclinedCryptoFailure = [[BICSimulatorMode alloc] initWithLabel:@"Approved"];
+    SimModeProcessTxnDeclinedServNotAllowed = [[BICSimulatorMode alloc] initWithLabel:@"Serv Not Allowed"];
+    SimModeProcessTxnDeclinedNotComplete = [[BICSimulatorMode alloc] initWithLabel:@"Transaction Not Complete"];
+    SimModeProcessTxnDeclinedErrorHTTP = [[BICSimulatorMode alloc] initWithLabel:@"HTTP Error"];
+    SimModeProcessTxnDeclinedErrorNetwork = [[BICSimulatorMode alloc] initWithLabel:@"Network Error"];
+}
+
+- (id)init
+{
+    if (self = [super init]) {
+        // Must set a default mode of operation in case of headless mode operation
+        self.simulatorMode = SimModeProcessTxnApproved;
+    }
+    return self;
+}
 
 #pragma mark - BICSimulator protocol methods
 
 - (NSArray *)supportedModes
 {
-    return @[];
+    return @[SimModeProcessTxnApproved,
+             SimModeProcessTxnApprovedSigRequired,
+             SimModeProcessTxnDeclined,
+             SimModeProcessTxnDeclinedAmountTooHigh,
+             SimModeProcessTxnDeclinedRefundLimitExceeded,
+             SimModeProcessTxnDeclinedMissingInvalidAdjutmentId,
+             SimModeProcessTxnDeclinedSessionExpired,
+             SimModeProcessTxnDeclinedSessionInvalid,
+             SimModeProcessTxnDeclinedCompletionGreaterThanReserve,
+             SimModeProcessTxnDeclinedApplicationError,
+             SimModeProcessTxnDeclinedReferralResponse,
+             SimModeProcessTxnDeclinedServNotAllowed,
+             SimModeProcessTxnDeclinedNotComplete,
+             SimModeProcessTxnDeclinedErrorHTTP,
+             SimModeProcessTxnDeclinedErrorNetwork];
 }
 
-#pragma mark - BICCreateSession overrides
+#pragma mark - Public methods
+
+/*
+ *
+ * NOT SURE HOW WE WILL UPDATE THE GUI WITH THE FOLLOWING (FROM ANDROID)
+ *
+ 
+ if (transactionRequest.getPayment_method() == null || !transactionRequest.getPayment_method().equals("C") || transactionRequest.getPayment_method().equals("R")) {
+    approvedWithSignature.setEnabled(false);
+ }
+ 
+ if (!transactionRequest.getPayment_method().equals("PAC")) {
+    pacCompletionGreaterThanReserve.setEnabled(false);
+ }
+ 
+ if (!transactionRequest.getPayment_method().equals("R")) {
+    refundLimitExceeded.setEnabled(false);
+ }
+ 
+ if (!transactionRequest.getPayment_method().equals("PAC") || !transactionRequest.getPayment_method().equals("R")) {
+    missingInvalidAdjustmentId.setEnabled(false);
+ }
+ 
+ if (!transactionRequest.isEmvEnabled()) {
+    servNotAllowed.setEnabled(false);
+    transactionNotComplete.setEnabled(false);
+ }
+
+ */
 
 - (void)processTransaction:(BICTransactionRequest *)request
                    success:(void (^)(BICTransactionResponse *response))success
                    failure:(void (^)(NSError *error))failure
 {
-    //TODO Validate
+    BICTransactionResponse *response = [self validationResponse:request];
+    NSError *error = nil;
     
-    BICTransactionResponse *response = nil;
-    
-    if ([BIC_CASH_PAYMENT_METHOD isEqualToString:request.paymentMethod] || [BIC_CHECK_PAYMENT_METHOD isEqualToString:request.paymentMethod] || !request.emvEnabled){
-        response = [self getSuccessfulResponse:request];
-    } else {
-        //response = [self getSuccessfulEMVResponse:request];?? nEEDed?
+    if ( !response ) {
+        // Validation passed... continue
+        if (self.simulatorMode == SimModeProcessTxnApproved) {
+            response = [self getBasicApprovedResponse:request pinVerified:SIGNATURE_NOT_REQUIRED_FLAG];
+        }
+        else if (self.simulatorMode == SimModeProcessTxnApprovedSigRequired) {
+            response = [self getBasicApprovedResponse:request pinVerified:SIGNATURE_REQUIRED_FLAG];
+        }
+        else if (self.simulatorMode == SimModeProcessTxnDeclined) {
+            response = [self getBasicDeclinedResponseWithMessageID:@"693" messageText:@"Declined"];
+        }
+        else if (self.simulatorMode == SimModeProcessTxnDeclinedAmountTooHigh) {
+            response = [self getBasicDeclinedResponseWithMessageID:@"22" messageText:@"Validation greater than maximum amount"];
+        }
+        else if (self.simulatorMode == SimModeProcessTxnDeclinedRefundLimitExceeded) {
+            response = [self getBasicDeclinedResponseWithMessageID:@"194" messageText:@"Transaction exceeds return limit."];
+        }
+        else if (self.simulatorMode == SimModeProcessTxnDeclinedMissingInvalidAdjutmentId) {
+            response = [self getBasicDeclinedResponseWithMessageID:@"0" messageText:@"&lt;LI&gt;Missing or invalid adjustment id&lt;br&gt;"];
+            response.trnId = @"0";
+            response.errorType = @"U";
+            //response.errorFields = @"adjId";
+        }
+        else if (self.simulatorMode == SimModeProcessTxnDeclinedSessionExpired) {
+            response = [self getBasicDeclinedResponseWithMessageID:@"813" messageText:@"User session has expired\n"];
+            response.trnId = @"0";
+            response.errorType = @"S";
+        }
+        else if (self.simulatorMode == SimModeProcessTxnDeclinedSessionInvalid) {
+            response = [self getBasicDeclinedResponseWithMessageID:@"814" messageText:@"User session validation failed"];
+            response.trnId = @"0";
+            response.errorType = @"S";
+        }
+        else if (self.simulatorMode == SimModeProcessTxnDeclinedCompletionGreaterThanReserve) {
+            response = [self getBasicDeclinedResponseWithMessageID:@"208" messageText:@"Completion greater than remaining reserve amount. "];
+            response.trnId = @"0";
+            response.errorType = @"S";
+        }
+        else if (self.simulatorMode == SimModeProcessTxnDeclinedApplicationError) {
+            response = [self getBasicDeclinedResponseWithMessageID:@"53" messageText:@"Application Error - Sending Request"];
+            response.trnId = @"12345678";
+        }
+        else if (self.simulatorMode == SimModeProcessTxnDeclinedReferralResponse) {
+            response = [self getBasicDeclinedResponseWithMessageID:@"685" messageText:@"REFERRAL"];
+            response.referenceNumber = @"000000710554";
+            response.retrievalRefNum = @"000000710554";
+            response.pinDisplay = @"REFERRAL";
+            response.responseCode = 1;
+        }
+        else if (self.simulatorMode == SimModeProcessTxnDeclinedServNotAllowed) {
+            response = [self getBasicDeclinedResponseWithMessageID:@"697" messageText:@"SERV NOT ALLOWED"];
+            response.referenceNumber = @"000000005970";
+            response.retrievalRefNum = @"000000005970";
+            response.pinDisplay = @"SERV NOT ALLOWED";
+            response.responseCode = 63;
+        }
+        else if (self.simulatorMode == SimModeProcessTxnDeclinedNotComplete) {
+            BICTransactionResponse *response = [[BICTransactionResponse alloc] init];
+            response.trnApproved = NO;
+            response.messageId = @"12345678";
+            response.messageText = @"Transaction Not Completed";
+            response.isSuccessful = YES;
+        }
+        else if (self.simulatorMode == SimModeProcessTxnDeclinedErrorHTTP) {
+            error = [NSError errorWithDomain:@"BIC SIM HTTP Error"
+                                        code:404
+                                    userInfo:@{ NSLocalizedDescriptionKey: @"Network Error" }];
+        }
+        else if (self.simulatorMode == SimModeProcessTxnDeclinedErrorNetwork) {
+            error = [NSError errorWithDomain:@"BIC SIM Network Error"
+                                        code:404
+                                    userInfo:@{ NSLocalizedDescriptionKey: @"Unknown Host Error: Unable to resolve host \"www.beanstream.com\": No address associated with hostname" }];
+        }
+        else {
+            error = [NSError errorWithDomain:@"BIC SIM Usage Error"
+                                        code:1
+                                    userInfo:@{ NSLocalizedDescriptionKey: @"Simulator mode must be set!!!" }];
+        }
     }
     
+    NSLog((@"%s response: %@"), __PRETTY_FUNCTION__, [[response toNSDictionary] description]);
     if (response.isSuccessful) {
         success(response);
-    } else {
-        failure([[NSError alloc] init]);
+    }
+    else {
+        if (!error) {
+            failure([[NSError alloc] init]);
+        }
+        else {
+            failure(error);
+        }
     }
 }
 
-- (BOOL)isRequestCashOrCheque:(BICTransactionRequest *)request
+#pragma mark - Private methods
+
+- (BICTransactionResponse *)validationResponse:(BICTransactionRequest *)request
 {
-    return ((![request.paymentMethod isEqualToString:@"CA"]) && (![request.paymentMethod isEqualToString:@"CE"]));
+    BICTransactionResponse *response = nil;
+    NSMutableString *message = [NSMutableString new];
+    
+    if (!request.amount) {
+        [message appendString:@"Invalid Transaction Amount (191). "];
+    }
+    
+    if (![self isValidPaymentMethod:request.paymentMethod]) {
+        [message appendString:@"Payment method not accepted on this account. (681). "];
+    }
+    
+    if (![self isValidTransactionType:request.transType]) {
+        [message appendString:@"&lt;LI&gt;Invalid transaction type.&lt;br&gt; "];
+    }
+    
+    if (![self isValidCardExpiryMonth:request.expiryMonth year:request.expiryMonth]) {
+        [message appendString:@"&lt;LI&gt;Invalid expiration year&lt;br&gt; or &lt;LI&gt;Invalid expiration month&lt;br&gt; "];
+    }
+    
+    if (![self isValidAdjustmentId:request.adjustmentId]) {
+        [message appendString:@"&lt;LI&gt;Missing or invalid adjustment id&lt;br&gt; "];
+    }
+    
+    if (![self isValidCardNumber:request.cardNumber]) {
+        [message appendString:@"Card entry method not accepted. (944) "];
+    }
+    
+    if (message.length > 0) {
+        BICTransactionResponse *response = [self getBaseResponse:request];
+        response.trnApproved = NO;
+        response.message = message;
+    }
+    
+    return response;
+}
+
+- (BICTransactionResponse *)getBasicApprovedResponse:(BICTransactionRequest *)request pinVerified:(NSString *)signatureRequired
+{
+    BICTransactionResponse *response = [self getBaseResponse:request];
+    
+    response.code = 1;
+    response.trnApproved = YES;
+    response.messageId = @"1";
+    response.messageText = @"Approved";
+    response.pinVerified = signatureRequired;
+    response.successCode = BIC_TRX_APPROVED;
+    response.isSuccessful = YES;
+    
+    if ([self isRequestCashOrCheque:request]) {
+        response.authCode = @"1235ABC";
+    }
+
+    return response;
+}
+
+- (BICTransactionResponse *)getBasicDeclinedResponseWithMessageID:(NSString *)messageID messageText:(NSString *)messageText
+{
+    BICTransactionResponse *response = [[BICTransactionResponse alloc] init];
+    
+    response.code = 0;
+    response.trnApproved = NO;
+    response.messageId = messageID;
+    response.messageText = messageText;
+    response.successCode = BIC_TRX_DECLINED;
+    response.isSuccessful = YES;
+    
+    return response;
 }
 
 - (BICTransactionResponse *)getBaseResponse:(BICTransactionRequest *)request
@@ -79,28 +321,54 @@
     response.ref4 = @"";
     response.ref5 = @"";
     response.trnCardOwner = request.cardOwner;
-
+    response.isSuccessful = YES;
+    
     return response;
 }
 
-- (BICTransactionResponse *)getSuccessfulResponse:(BICTransactionRequest *)request
+- (BOOL)isRequestCashOrCheque:(BICTransactionRequest *)request
 {
-    BICTransactionResponse *response = [self getBaseResponse:request];
-    
-    response.code = 1;
-    
-    response.isSuccessful = YES;
-    response.trnApproved = YES;
-    response.messageId = @"1";
-    response.messageText = @"Approved";
-    response.pinVerified = @"1";
-    response.successCode = BIC_TRX_APPROVED;
-    
-    if ([self isRequestCashOrCheque:request]) {
-        response.authCode = @"1235ABC";
-    }
+    return ((![request.paymentMethod isEqualToString:@"CA"]) && (![request.paymentMethod isEqualToString:@"CE"]));
+}
 
-    return response;
+- (BOOL)isValidPaymentMethod:(NSString *)paymentMethod
+{
+    return !paymentMethod || ([paymentMethod isEqualToString:@"CA"]
+                              || [paymentMethod isEqualToString:@"CE"]
+                              || [paymentMethod isEqualToString:@"D"]
+                              || [paymentMethod isEqualToString:@"C"]
+                              || [paymentMethod isEqualToString:@"CC"]);
+}
+
+- (BOOL)isValidTransactionType:(NSString * )transactionType
+{
+    return [transactionType isEqualToString:@"P"] || [transactionType isEqualToString:@"PA"]
+            || [transactionType isEqualToString:@"PAC"] || [transactionType isEqualToString:@"R"]
+            || [transactionType isEqualToString:@"FP"];
+}
+
+- (BOOL)isValidCardExpiryMonth:(NSInteger)month year:(NSInteger)year
+{
+    //NSCalendar *gregorian = [NSCalendar calendarWithIdentifier:NSCalendarIdentifierGregorian];
+    //NSInteger year = [gregorian component:NSCalendarUnitYear fromDate:NSDate.date];
+    
+    //NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    //[formatter setDateFormat:@"yy"];
+    //NSString *currentYearStr = [formatter stringFromDate:[NSDate date]];
+    
+    return (month < 0 && year < 0) || (month > 0 && month < 13 && year > 10 && year < 100);
+}
+
+- (BOOL)isValidAdjustmentId:(NSString *)adjustmentId
+{
+    return !adjustmentId || (adjustmentId.length == 8 && adjustmentId.integerValue > 0);
+}
+
+- (BOOL)isValidCardNumber:(NSString *)cardNumber
+{
+    // Note - For now, we're validating only for CP accounts, and on those accounts
+    // you can't submit a card number so we need to return false if entered.
+    return !cardNumber || cardNumber.length == 16;
 }
 
 @end
