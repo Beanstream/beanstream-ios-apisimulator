51456b02eac34ed903e07c1ef76b431a87ca8eb4
Sven Resch <sven.resch@beanstream.com>
2016-01-19 11:05:33 -0800

    MDI-437. Implemented the Authenticate Session Simulator to be equivalent to Android.

--- APISimulator/Simulators/BICAuthenticateSessionSimulator.m
+++ APISimulator/Simulators/BICAuthenticateSessionSimulator.m
@@ -11,21 +11,94 @@
 
 @implementation BICAuthenticateSessionSimulator
 
+static NSString *AUTHENTICATE_SESSION_VERSION_NUMBER = @"1.0";
+
+static BICSimulatorMode *SimulatorModeAuthenticateSessionPass = nil;
+static BICSimulatorMode *SimulatorModeAuthenticateSessionInvalid = nil;
+
 @synthesize simulatorMode, interactive;
+
+#pragma mark - Initialization methods
+
++ (void)initialize
+{
+    SimulatorModeAuthenticateSessionPass = [[BICSimulatorMode alloc] initWithLabel:@"Pass"];
+    SimulatorModeAuthenticateSessionInvalid = [[BICSimulatorMode alloc] initWithLabel:@"Invalid Session ID"];
+}
+
+- (id)init
+{
+    if (self = [super init]) {
+        // Must set a default mode of operation in case of headless mode operation
+        self.simulatorMode = SimulatorModeAuthenticateSessionPass;
+    }
+    return self;
+}
 
 #pragma mark - BICSimulator protocol methods
 
 - (NSArray *)supportedModes
 {
-    return @[];
+    return @[SimulatorModeAuthenticateSessionPass,
+             SimulatorModeAuthenticateSessionInvalid];
 }
 
-#pragma mark - BICCreateSession overrides
+#pragma mark - Public methods
 
 - (void)authenticateSession:(void (^)(BICAuthenticateSessionResponse *response))success
                     failure:(void (^)(NSError *error))failure
 {
-    //TODO Validate
+    BICAuthenticateSessionResponse *response = nil;
+    NSError *error = nil;
+    
+    if (self.simulatorMode == SimulatorModeAuthenticateSessionPass) {
+        response = [self createSuccessfulResponse];
+    }
+    else if (self.simulatorMode == SimulatorModeAuthenticateSessionInvalid) {
+        response = [self createInvalidSessionIdResponse];
+    }
+    else {
+        error = [NSError errorWithDomain:@"BIC SIM Usage Error"
+                                    code:1
+                                userInfo:@{ NSLocalizedDescriptionKey: @"Simulator mode must be set!!!" }];
+    }
+    
+    NSLog((@"%s response: %@"), __PRETTY_FUNCTION__, [[response toNSDictionary] description]);
+    if (response.isSuccessful) {
+        success(response);
+    }
+    else {
+        if (!error) {
+            failure([[NSError alloc] init]);
+        }
+        else {
+            failure(error);
+        }
+    }
+}
+
+#pragma mark - Private methods
+
+- (BICAuthenticateSessionResponse *)createSuccessfulResponse
+{
+    BICAuthenticateSessionResponse *response = [[BICAuthenticateSessionResponse alloc] init];
+    response.code = 1;
+    response.version = AUTHENTICATE_SESSION_VERSION_NUMBER;
+    response.message = @"Session authenticated";
+    response.isSuccessful = YES;
+    
+    return response;
+}
+
+- (BICAuthenticateSessionResponse *)createInvalidSessionIdResponse
+{
+    BICAuthenticateSessionResponse *response = [[BICAuthenticateSessionResponse alloc] init];
+    response.code = 7;
+    response.version = AUTHENTICATE_SESSION_VERSION_NUMBER;
+    response.message = @"Invalid authentication credentials";
+    response.isSuccessful = YES;
+    
+    return response;
 }
 
 @end
