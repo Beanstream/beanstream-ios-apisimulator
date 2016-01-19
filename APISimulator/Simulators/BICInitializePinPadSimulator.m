f526e765e041e4dbd7fc23dd2a18cb3c4f9e90ba
Sven Resch <sven.resch@beanstream.com>
2016-01-18 16:44:54 -0800

    MDI-438. Implemented the Initialize Pin Pad simulator to an equivalent state compared to Android.

--- APISimulator/Simulators/BICInitializePinPadSimulator.m
+++ APISimulator/Simulators/BICInitializePinPadSimulator.m
@@ -11,42 +11,125 @@
 
 @implementation BICInitializePinPadSimulator
 
+static NSString *INITIALIZE_PINPAD_VERSION_NUMBER = @"1.0";
+
+static BICSimulatorMode *SimulatorModeInitializePinPadPass = nil;
+static BICSimulatorMode *SimulatorModeInitializePinPadFail = nil;
+static BICSimulatorMode *SimulatorModeInitializePinPadUpdate = nil;
+static BICSimulatorMode *SimulatorModeInitializePinPadInvalidSession = nil;
+
 @synthesize simulatorMode, interactive;
+
+#pragma mark - Initialization methods
+
++ (void)initialize
+{
+    SimulatorModeInitializePinPadPass = [[BICSimulatorMode alloc] initWithLabel:@"Pass"];
+    SimulatorModeInitializePinPadFail = [[BICSimulatorMode alloc] initWithLabel:@"Fail"];
+    SimulatorModeInitializePinPadUpdate = [[BICSimulatorMode alloc] initWithLabel:@"Update"];
+    SimulatorModeInitializePinPadInvalidSession = [[BICSimulatorMode alloc] initWithLabel:@"Invalid Session"];
+}
+
+- (id)init
+{
+    if (self = [super init]) {
+        // Must set a default mode of operation in case of headless mode operation
+        self.simulatorMode = SimulatorModeInitializePinPadPass;
+    }
+    return self;
+}
 
 #pragma mark - BICSimulator protocol methods
 
 - (NSArray *)supportedModes
 {
-    return @[];
+    return @[SimulatorModeInitializePinPadPass,
+             SimulatorModeInitializePinPadFail,
+             SimulatorModeInitializePinPadUpdate,
+             SimulatorModeInitializePinPadInvalidSession];
 }
 
-#pragma mark - BICCreateSession overrides
+#pragma mark - Public methods
 
 - (void)initializePinPad:(void (^)(BICInitPinPadResponse *response))success
                  failure:(void (^)(NSError *error))failure
 {
-    //TODO Validate
+    BICInitPinPadResponse *response = nil;
+    NSError *error = nil;
     
-    BICInitPinPadResponse *response = [self getSuccessfulResponse];
+    if (self.simulatorMode == SimulatorModeInitializePinPadPass) {
+        response = [self createInitializedResponse];
+    }
+    else if (self.simulatorMode == SimulatorModeInitializePinPadFail) {
+        response = [self createInitializeFailedResponse];
+    }
+    else if (self.simulatorMode == SimulatorModeInitializePinPadUpdate) {
+        response = [self createInitializedUpdateRequired];
+    }
+    else if (self.simulatorMode == SimulatorModeInitializePinPadInvalidSession) {
+        //response = [self
+    }
+    else {
+        error = [NSError errorWithDomain:@"BIC SIM Usage Error"
+                                    code:1
+                                userInfo:@{ NSLocalizedDescriptionKey: @"Simulator mode must be set!!!" }];
+    }
     
+    NSLog((@"%s response: %@"), __PRETTY_FUNCTION__, [[response toNSDictionary] description]);
     if (response.isSuccessful) {
         success(response);
-    } else {
-        failure([[NSError alloc] init]);
+    }
+    else {
+        if (!error) {
+            failure([[NSError alloc] init]);
+        }
+        else {
+            failure(error);
+        }
     }
 }
 
-- (BICInitPinPadResponse *)getSuccessfulResponse
+- (BICInitPinPadResponse *)createInitializedResponse
 {
     BICInitPinPadResponse *response = [[BICInitPinPadResponse alloc] init];
-    
-    response.isSuccessful = YES;
-    
     response.code = 1;
-    response.version = @"1.0";
     response.terminalId = @"12345678";
     response.updateKeyFile = NO;
-    
+    response.version = INITIALIZE_PINPAD_VERSION_NUMBER;
+    response.isSuccessful = YES;
+    return response;
+}
+
+- (BICInitPinPadResponse *)createInitializeFailedResponse
+{
+    BICInitPinPadResponse *response = [[BICInitPinPadResponse alloc] init];
+    response.code = 1;
+    response.terminalId = @"";
+    //initCardReaderResponse.setInitialized(false); // why is there no iOS equivalent????
+    response.updateKeyFile = NO;
+    response.version = INITIALIZE_PINPAD_VERSION_NUMBER;
+    response.isSuccessful = YES;
+    return response;
+}
+
+- (BICInitPinPadResponse *)createInitializedUpdateRequired {
+    BICInitPinPadResponse *response = [[BICInitPinPadResponse alloc] init];
+    response.code = 1;
+    response.terminalId = @"";
+    //initCardReaderResponse.setInitialized(true);
+    response.updateKeyFile = YES;
+    response.version = INITIALIZE_PINPAD_VERSION_NUMBER;
+    response.isSuccessful = YES;
+    return response;
+}
+
+- (BICInitPinPadResponse *)createInvalidSessionResponse
+{
+    BICInitPinPadResponse *response = [[BICInitPinPadResponse alloc] init];
+    response.code = 7;
+    response.message = @"Authentication failed";
+    response.version = INITIALIZE_PINPAD_VERSION_NUMBER;
+    response.isSuccessful = YES;
     return response;
 }
 
